94a1c433be5b25ce349cd9fa0aa6a01884fe99a6
falzuro <felipealzuro@gmail.com>
2025-02-10 23:33:27 -0500

    subirproyecto4

--- src/components/button.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = (props) => {
-  return <button {...props} className={`px-4 py-2 bg-blue-500 text-white rounded ${props.className || ''}`} />
-}
